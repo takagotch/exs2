31a2a1b6153563bfc03e6b9daa1322a0feaa025f
takagotch <dyaccb@gmail.com>
2018-04-07 02:43:25 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- node/ticker.ex
+++ node/ticker.ex
@@ -27,3 +27,22 @@ defmodule Ticker do
   end
 end
 
+defmodule Client do
+  def start do
+    pid = spawn(__MODULE__, :receiver, [])
+    Ticker.register(pid)
+  end
+
+  def receiver do
+    receive do
+      { :tick } ->
+        IO.puts "tock in client"
+	receiver
+    end
+  end
+end
+
+
+
+
+
