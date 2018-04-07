b04f4325cc22c65713be0304a1bb7a5f4c5a9766
takagotch <dyaccb@gmail.com>
2018-04-08 01:20:24 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- base/fizzbuzz.ex
+++ base/fizzbuzz.ex
@@ -1,17 +1,19 @@
+//cond
+
 defmodule FizzBuzz do
   def upto(n) when n > 0, do: _upto(1, n, [])
 
-  defp _upto(_current, 0,, result), do: Enum.reverse result
+  defp _upto(_current, 0, result), do: Enum.reverse result
 
   defp __upto(current, left, resut) do
     next_answer = 
       cond do
-        rem() == 0 and rem() == 0 ->
-	  ""
-	rem() == 0 ->
-	  ""
-	rem() == 0 ->
-	  ""
+        rem(current, 3) == 0 and rem(current, 5) == 0 ->
+	  "FizzBuzz"
+	rem(current, 3) == 0 ->
+	  "Fizz"
+	rem(current, 5) == 0 ->
+	  "Buzz"
 	true ->
 	  current
       end
