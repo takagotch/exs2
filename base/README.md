618f7e815edae245ed677b7f5508b92967c11104
takagotch <dyaccb@gmail.com>
2018-04-08 02:03:33 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ base/README.md
@@ -0,0 +1,13 @@
+
+
+starts_with?(str, prefix | [ prefixes ])
+strip(str)
+strip(str, character)
+upcase(str)
+valid_character?(str)
+
+
+
+
+
+
--- base/cmd.sh
+++ base/cmd.sh
@@ -28,4 +28,53 @@ raise RuntimeError, message: "override message"
 
 
 
+dps = "0x/0y"
+String.lenght dps
+byte_size dps
+String.at(dps, 0)
+String.codepoints(dps)
+String.split(dps, "/")
+String.at("Dog", 0)
+String.at("Dob", 0)
+String.capitalize "ecole"
+String.capitalize "iiiii"
+String.codepoints("tky Do g")
+String.downcase "0 RsteD"
+String.duplicate "Hi!", 3
+String.ends_with? "string", ["elix", "stri", "ring"]
+String.first "Dog"
+String.codepoints "noe\u3081"
+String.graphemes "noe\u03081"
+String.last "Dog"
+String.length "0x/0y"
+String.ljust("str", 5)
+String.lstrip "\t\f Hello\t\n"
+String.lstrip "!!!SALE!!!", ?!
+String.printable? "tky"
+String.printable? "\x{0000} a null"
+String.replace "str str", "at", "Osaka"
+String.replace "str str", "at", "Osaka", global: false
+String.replace "str str", "at", "Osaka", insert_replacesed: 0
+String.replace "str str", "at", "Osaka", insert_replaced: [0,2]
+String.reverse "dog"
+String.reverse ""
+String.rjust("cat", 5, ?>)
+String.rstrip("line \r\n")
+String.rstrip "!!!SALE!!!", ?!
+String.slice "sti str", 4, 3
+String.slice "str str", -3, 3
+String.split "str str "
+String.split " str str ", "t"
+String.split "str str", ~r([ae])
+String.split "str str", ~r([ae]), parts: 2
+String.starts_with? "string", ["elix", "stri", "ring"]
+String.strip "\t Hello \r\n"
+String.strip "!!!SALE!!!", ?!
+String.upcase "TKY"
+String.valid_character? "D"
+String.valid_character? "Dog"
+center(["cat", "zebra", "elephant"])
+
+
+
 
