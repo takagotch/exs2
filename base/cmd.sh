f0ab9a3f3084b4b39ee6d290eee1f5a092492c3e
takagotch <dyaccb@gmail.com>
2018-04-08 15:02:17 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- base/cmd.sh
+++ base/cmd.sh
@@ -312,13 +312,23 @@ name = "Tky"
 for name <- [ "str", "str" ], do: String.upcase(name)
 name
 
-for x <- ~w{}, into: %{}, do: { x, String.upcase(x) }
-for x <- ~w{}, into: Map.new, do: { x, String.upcase(x) }
-for x <- ~w{}, into: %{}, do: { x, String.upcase(x) }
+for x <- ~w{ str str }, into: %{}, do: { x, String.upcase(x) }
+for x <- ~w{ str str }, into: Map.new, do: { x, String.upcase(x) }
+for x <- ~w{ str str }, into: %{"str" => "str"}, do: { x, String.upcase(x) }
 
-for x <- ~w{}, into: IO.stream(), do: ""
+for x <- ~w{ str str }, into: IO.stream(:stdio,:line), do: "<<#[x]>>\n"
 
+tax_rates [ NC: 0.075, TX: 0.08 ]
 
+orders = [
+  [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+  [ id: 124, ship_to: :OK, net_amount: 35.00 ],
+  [ id: 125, ship_to: :TX, net_amount: 24.00 ],
+  [ id: 126, ship_to: :TX, net_amount: 44.80],
+  [ id: 127, ship_to: :NC, net_amount: 25.00 ],
+  [ id: 128, ship_to: :MA, net_amount: 10.00 ],
+  [ id: 129, ship_to: :CA, net_amount: 102.00]
+  [ id: 130, ship_to: :NC, net_amount:  50.00] ]
 
 
 
