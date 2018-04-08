485fcec239318537e31156873b1a977aa8e54625
takagotch <dyaccb@gmail.com>
2018-04-08 14:06:57 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- base/cmd.sh
+++ base/cmd.sh
@@ -237,7 +237,15 @@ MyList.flatten([ 1, [ 2, 3, [4] ], 5, [[[6]]]])
 
 
 s = Stream.map [1, 3, 5, 7], &(&1 + 1)
+Enum.to_list s
 
+squares = Stream.map[1, 2, 3, 4], &(&1*&1)
+plus_ones = Stream.map squares, &(&1+1)
+odds = Stream.filter plus_ones, fn x -> rem(x,2) == 1 end
+Enum.to_list odds
+
+Enum.map() |> Enum.take(5)
+Stream.map() |> Enum.take(5)
 
 
 
