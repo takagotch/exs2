c009b3eec4f9c1072388468296787d8d7cfa39f2
takagotch <dyaccb@gmail.com>
2018-04-09 23:11:01 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ fr/cmd.sh
@@ -0,0 +1,29 @@
+grep Elixir *.pml | wc -l
+
+ech Hello
+ies
+3+ 4
+String.reverse "sr"
+5 *
+6
+
+
+h
+
+h IO
+h (IO)
+
+h IO.puts
+i 123
+i "str"
+i %{ name: "str", likes: "Str" }
+i Map
+h IEx.configure
+
+
+
+elixir fr.exs
+iex
+c "fr.exs"
+
+
