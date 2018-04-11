69b4e03d2205e5877f695ea63e2c266f1e35dd9c
takagotch <dyaccb@gmail.com>
2018-04-11 15:58:12 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ macro/dumper.exs
@@ -0,0 +1,36 @@
+defmodule My do
+  defmacro macro(params) do
+    IO.inspect param
+  end
+end
+
+defmodule Test do
+  require My
+
+  My.macro :atom
+  My.macro 1
+  My.macro 1.0
+  My.macro [1,2,3]
+  My.macro "binaries"
+  My.macro { 1, 2 }
+  My.macro do: 1
+
+  My.macro { 1,2,3,4,5 }
+
+  My.macro do: ( a = 1; a+a )
+  #
+  #
+  #
+  #
+
+  My.macro do
+  else
+  end
+  #
+  #
+
+end
+
+
+
+
